f63f3730da8cd47776eaf617cbebb25d536dbd37
zhnnnnn <475816908@qq.com>
2016-05-31 15:29:21 +0800

    Initial Commit

---
+++ navigationTitleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  navigationTitleView
+//
+//  Created by zhn on 16/5/31.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ navigationTitleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  navigationTitleView
+//
+//  Created by zhn on 16/5/31.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
